fd3e885622aa30491b29f1cc7bc2cb1647fd9fb8
Jacob Davis <jacobdofficial@gmail.com>
2024-01-17 08:29:46 -0700

    Initial Commit

---
+++ iTunesSearch/ItemCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCollectionViewCell.swift
+//  iTunesSearch
+//
+//  Created by Jacob Davis on 1/9/24.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell, ItemDisplaying {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var itemImageView: UIImageView!
+    
+    
+}
